fffee246a2ff889751de68decf5da8d8785edef7
adityakulkarni3618 <adityakulkarni3618@gmail.com>
2026-01-28 10:34:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.getElementById("issueForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  document.getElementById("message").innerText =
+    "Issue submitted successfully! (Firebase integration coming soon)";
+});
 
